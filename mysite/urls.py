8872eb8f6cb1751342a24c18ea070b3cf89414db
Mark E. DeYoung <mark.deyoung@usma.edu>
2015-04-22 18:03:55 -0400

    Redirect root URL to a specific app URL

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,12 +1,10 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-
+from django.http import HttpResponseRedirect
 from django.contrib.auth import views as auth_views
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'mysite.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+    url(r'^$', lambda r: HttpResponseRedirect('polls/')),
     url(r'^polls/', include('polls.urls', namespace="polls")),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^accounts/login/$', auth_views.login),
